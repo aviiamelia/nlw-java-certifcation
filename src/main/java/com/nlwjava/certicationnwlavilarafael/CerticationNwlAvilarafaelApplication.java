e0ff16bffcc57cdf1d5480cf369ff120d9f02fa9
rafael <rafael.zoe26@gmail.com>
2024-02-05 23:03:38 -0300

    feat: initial commit

---
+++ src/main/java/com/nlwjava/certicationnwlavilarafael/CerticationNwlAvilarafaelApplication.java
@@ -0,0 +1,13 @@
+package com.nlwjava.certicationnwlavilarafael;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CerticationNwlAvilarafaelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CerticationNwlAvilarafaelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nlwjava/certicationnwlavilarafael/CerticationNwlAvilarafaelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nlwjava.certicationnwlavilarafael;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CerticationNwlAvilarafaelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
