201b62d7e034401696d4fab48b8b59697ff3d335
rafael <rafael.zoe26@gmail.com>
2024-02-08 02:14:35 -0300

    feat: controller implementation

--- src/main/java/com/nlwjava/certicationnwlavilarafael/modules/studends/controllers/StudentController.java
+++ src/main/java/com/nlwjava/certicationnwlavilarafael/modules/studends/controllers/StudentController.java
@@ -6,15 +6,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nlwjava.certicationnwlavilarafael.modules.studends.dto.StudentCertificationAnswerDTO;
 import com.nlwjava.certicationnwlavilarafael.modules.studends.dto.VerifyHasCertificationDTO;
-import com.nlwjava.certicationnwlavilarafael.modules.studends.useCases.VerifyHasCertificationUseCases;
+import com.nlwjava.certicationnwlavilarafael.modules.studends.useCases.StudentCertificationAnswersUseCase;
+import com.nlwjava.certicationnwlavilarafael.modules.studends.useCases.VerifyHasCertificationUseCase;
 
 @RestController
 @RequestMapping("/students")
 public class StudentController {
 
     @Autowired
-    private VerifyHasCertificationUseCases verifyHasCertificationUseCase;
+    private VerifyHasCertificationUseCase verifyHasCertificationUseCase;
+
+    @Autowired
+    private StudentCertificationAnswersUseCase studentCertificationAnswersUseCase;
 
     @PostMapping("/verifyHasCertification")
     public String verifyHasCertification(@RequestBody VerifyHasCertificationDTO verifyHasCertification) {
@@ -24,5 +29,11 @@ public class StudentController {
             return "nao pode fazer";
         }
         return "pode fazer";
+    }
+
+    @PostMapping("/certification/answer")
+    public StudentCertificationAnswerDTO certificationAnswer(
+            @RequestBody StudentCertificationAnswerDTO studentCertificationAnswerDTO) {
+        return studentCertificationAnswersUseCase.execute((studentCertificationAnswerDTO));
     }
 }
